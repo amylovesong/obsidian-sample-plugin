afeb1129a53745dff9d11639265c0a08c3b69db9
amylovesong <amylovesong.sun@gmail.com>
2025-11-15 22:24:07 +0800

    Markdown code block processor

--- main.ts
+++ main.ts
@@ -123,6 +123,24 @@ export default class MyPlugin extends Plugin {
 				}
 			}
 		});
+
+		// Use Markdown code block processor to add a new code block type.
+		this.registerMarkdownCodeBlockProcessor('csv', (source, el, ctx) => {
+			const rows = source.split('\n').filter((row) => row.length > 0);
+
+			const table = el.createEl('table');
+			const body = table.createEl('tbody');
+
+			for (let i = 0; i < rows.length; i++) {
+				const cols = rows[i].split(',');
+
+				const row = body.createEl('tr')
+
+				for (let j = 0; j < cols.length; j++) {
+					row.createEl('td', { text: cols[j] });
+				}
+			}
+		});
 	}
 
 	onunload() {
